d9a4a58b8c0173a641951ab0796eb096a2dc45e6
Brayan Vinicius Jordan <brayan.11fera@gmail.com>
2024-01-09 19:50:10 -0300

    feat: Configurando biblioteca de esitlos (stitches) que facilita a implementação de estilos em aplicações SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,30 @@
+import { styled } from "../styles";
+
+const Button = styled("button", {
+  backgroundColor: "$rocketseat",
+  borderRadius: 4,
+  border: 0,
+  padding: "4px 8px",
+
+  span: {
+    fontWeight: "bold",
+  },
+
+  "&:hover": {
+    backgroundColor: "red",
+  },
+});
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Button>
+        <span>Teste</span>
+        Enviar
+      </Button>
+      <span>
+        agora foi
+      </span>
+    </>
+  );
 }
